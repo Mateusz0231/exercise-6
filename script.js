fbac69e1afb9a211f4b39a80d98e8939c0e2ccfc
Mateusz <100692029+Mateusz0231@users.noreply.github.com>
2022-05-24 11:32:42 +0000

    zad1 działa

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 (function () {
-  //TODO
+ const ex1 = document.getElementById('ex1_button')
+  const ex1c = document.getElementById('ex1_content')
+
+  ex1.addEventListener("click", function () {
+    var data ="";
+    for (var i=0;i<10;i++){
+      if(i!=0)
+        data +=","
+      data +=i.toString();
+    }
+    ex1c.innerHTML = data;
+  })
+const ex2 = document.querySelector('ex2_text')
+  const ex2c = document.querySelector('ex2_content')
+  
+  ex2.addEventListener("focus", function () {
+    ex2c.innerHTML = "";
+  });
+  ex2.addEventListener("blur",function(){
+            ex2c.innerHTML = "";
+        });
+   
+  
+ 
 })();
